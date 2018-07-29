5ce5ddb9f6c9d5088087ae5b0ed883bdd1cd7928
trifon dardzhonov <tdardzhonov@centroida.ai>
2018-07-29 20:50:30 +0300

    clean up

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -18,65 +18,14 @@ export class AppComponent {
   }
 
   public isMenuOpen = false;
-
-  // Data generated from http://www.bikeforums.net/professional-cycling-fans/1113087-2017-tour-de-france-gpx-tcx-files.html
-  public lineChartData = [
-    [0.0, 225],
-    [0.1, 226],
-    [0.2, 228],
-    [0.3, 228],
-    [0.4, 229],
-    [0.5, 229],
-    [0.6, 230],
-    [0.7, 234],
-    [0.8, 235],
-    [0.9, 236],
-    [1.0, 235],
-    [1.1, 232],
-    [1.2, 228],
-    [1.3, 223],
-    [1.4, 218],
-    [1.5, 214],
-    [1.6, 207],
-    [1.7, 202],
-    [1.8, 198],
-    [1.9, 196],
-  ];
-
-  public pieChartData = [{
-    name: 'Chrome',
-    y: 61.41,
-  }, {
-    name: 'Internet Explorer',
-    y: 11.84
-  }, {
-    name: 'Firefox',
-    y: 10.85
-  }, {
-    name: 'Edge',
-    y: 4.67
-  }, {
-    name: 'Safari',
-    y: 4.18
-  }, {
-    name: 'Sogou Explorer',
-    y: 1.64
-  }, {
-    name: 'Opera',
-    y: 1.6
-  }, {
-    name: 'QQ',
-    y: 1.2
-  }, {
-    name: 'Other',
-    y: 2.61
-  }];
-
   public nodes: SNMPNode[] = [];
   public loading = false;
 
   public select(endpoint: SNMPEndpoint): void {
     this.selectedEndpoint = endpoint;
+    if (!this.isMenuOpen) {
+      this.isMenuOpen = true;
+    }
 
     this.loading = true;
     this.snmpService.snmpEndPointDetails(endpoint).subscribe(n => {
